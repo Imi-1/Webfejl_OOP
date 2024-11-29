a3de8ba81bc3c2ff037453de8262ee1a8d21fe53
Borsos Imre <borsos.imre@BOLYAI.LOCAL>
2024-11-29 15:20:36 +0100

    gomszab

---
+++ basics/scripts.js
@@ -0,0 +1,36 @@
+function Player(nickname){
+    this.nickname=nickname;
+    this.palyedMatch=0;
+}
+
+Player.prototype.play=
+function(){
+this.palyedMatch++;
+console.log(this.nickname,this.palyedMatch)
+}
+
+
+Player.prototype.getTierLevel=
+function(){
+    if(this.palyedMatch < 4){
+        return"A";
+    }
+    else if(this.palyedMatch < 7 && this.palyedMatch > 3){
+        return"B";
+    }
+    else{
+        return"C";
+    }
+    
+}
+const gomszab=new Player("Gomszab");
+console.log(gomszab);
+gomszab.play()
+gomszab.play()
+gomszab.play()
+gomszab.play()
+
+console.log(gomszab.getTierLevel())
+
+
+
