e187dc5f0032052b5480c802ba6878a85b6783d7
Imi-1 <imreborsos2007@gmail.com>
2024-11-29 23:22:12 +0100

    hazi?

--- basics/scripts.js
+++ basics/scripts.js
@@ -75,8 +75,91 @@ class Student{
     }
 }
 
+
+
 const geza=new Person("Géza");
 
 Object.setPrototypeOf(Student.prototype, Person.prototype);
 
 console.log(geza.getName());
+
+
+// class Animal{
+//     constructor(hangja,csinalja){
+//         this.hangja=hangja;
+//         this.csinalja=csinalja;
+//     }
+
+//     kiir() {
+//         console.log(`A/Az ${this.nev} azt mondja, hogy "${this.hangja}" és "${this.csinalja}".`);
+//     }
+// }
+
+// class Bird{
+//     constructor(nev) {
+//         Animal.call(this,"csirip","repül")
+//         this.nev=nev;
+//     }
+// }
+// Object.setPrototypeOf(Bird.prototype, Animal.prototype);
+
+
+// class Mammal{
+//     constructor(nev) {
+//         Animal.call(this,"woof","jár")
+//         this.nev=nev;
+//     }
+// }
+// Object.setPrototypeOf(Mammal.prototype, Animal.prototype);
+
+
+
+
+// const kismadar=new Bird("cigánycsuk");
+// const nagykutya=new Mammal("bulldog");
+
+// kismadar.kiir();
+// nagykutya.kiir();
+
+
+
+
+class Animal{
+    
+}
+
+class Bird{
+    constructor(nev,hangja,csinalja){
+        this.hangja="csirip";
+        this.csinalja="repül";
+        this.nev=nev;
+    }
+
+    kiir() {
+        console.log(`A/Az ${this.nev} azt mondja, hogy "${this.hangja}" és "${this.csinalja}".`);
+    }
+}
+Object.setPrototypeOf(Bird.prototype, Animal.prototype);
+
+
+class Mammal{
+    constructor(nev,hangja,csinalja,){
+        this.hangja="woof";
+        this.csinalja="jár";
+        this.nev=nev;
+    }
+
+    kiir() {
+        console.log(`A/Az ${this.nev} azt mondja, hogy "${this.hangja}" és "${this.csinalja}".`);
+    }
+}
+Object.setPrototypeOf(Mammal.prototype, Animal.prototype);
+
+
+
+
+const kismadar=new Bird("cigánycsuk");
+const nagykutya=new Mammal("bulldog");
+
+kismadar.kiir();
+nagykutya.kiir();
