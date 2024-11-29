0b2d8891c736dc857f19d07f95aa3a7708629170
Borsos Imre <borsos.imre@BOLYAI.LOCAL>
2024-11-29 15:30:46 +0100

    masik gomszab cuccos

--- basics/scripts.js
+++ basics/scripts.js
@@ -1,27 +1,52 @@
-function Player(nickname){
-    this.nickname=nickname;
-    this.palyedMatch=0;
-}
+// function Player(nickname){
+//     this.nickname=nickname;
+//     this.palyedMatch=0;
+// }
 
-Player.prototype.play=
-function(){
-this.palyedMatch++;
-console.log(this.nickname,this.palyedMatch)
-}
+// Player.prototype.play=
+// function(){
+// this.palyedMatch++;
+// console.log(this.nickname,this.palyedMatch)
+// }
 
 
-Player.prototype.getTierLevel=
-function(){
-    if(this.palyedMatch < 4){
-        return"A";
-    }
-    else if(this.palyedMatch < 7 && this.palyedMatch > 3){
-        return"B";
-    }
-    else{
-        return"C";
-    }
+// Player.prototype.getTierLevel=
+// function(){
+//     if(this.palyedMatch < 4){
+//         return"A";
+//     }
+//     else if(this.palyedMatch < 7 && this.palyedMatch > 3){
+//         return"B";
+//     }
+//     else{
+//         return"C";
+//     }
     
+// }
+
+
+class Player{
+    constructor(nickname){
+        this.nickname=nickname;
+        this.palyedMatch=0;
+    }
+
+    play(){
+        this.palyedMatch++;
+        console.log(this.nickname,this.palyedMatch)
+    }
+
+    getTierLevel(){
+        if(this.palyedMatch < 4){
+                    return"A";
+                }
+                else if(this.palyedMatch < 7 && this.palyedMatch > 3){
+                    return"B";
+                }
+                else{
+                    return"C";
+                }
+    }
 }
 const gomszab=new Player("Gomszab");
 console.log(gomszab);
